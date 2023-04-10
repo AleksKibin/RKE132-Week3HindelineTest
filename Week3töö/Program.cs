6e996e1139f3019fca45460b7527acecfd38463f
AleksK <kibinaleks@gmail.com>
2023-04-10 20:52:36 +0300

    week3 töö

---
+++ Week3töö/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Palun sisestage oma hinne:");
+char userGrade;
+if (userGrade == 'A')
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userGrade == 'B')
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userGrade == 'C')
+{
+    Console.WriteLine("Hea!");
+}
+else if (userGrade == 'D')
+{
+    Console.WriteLine("Rahuldav!");
+}
+else if (userGrade == 'E')
+{
+    Console.WriteLine("Kasin!");
+}
+else if (userGrade == 'F')
+{
+    Console.WriteLine("Puudulik!");
+}
+else
+{
+    Console.WriteLine("Vale väärtus");
+}
